9b5d7b04a5be75726ff5fbc015d27e217f8a629a
kalru <47431193+kalru@users.noreply.github.com>
2023-01-12 16:24:55 +0200

    Show files changed stat as decription in menu

--- git-worktree.go
+++ git-worktree.go
@@ -41,6 +41,17 @@ func get_worktrees() []string {
 	return worktrees[:len(worktrees)-1]
 }
 
+func get_files_changed(branch string) string {
+	// the last line of the command: git diff --stat
+	cmd := exec.Command("sh", "-c", fmt.Sprintf("git diff %s --stat", branch))
+	out, err := cmd.Output()
+	if err != nil {
+		log.Printf("Error running command: %s", err)
+	}
+	lines := strings.Split(string(out[:]), "\n")
+	return lines[len(lines)-2]
+}
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
@@ -79,10 +90,11 @@ func main() {
 
 	items := []list.Item{}
 
-	for i, tree := range get_worktrees() {
+	for _, tree := range get_worktrees() {
 		re_branch := regexp.MustCompile(`\[([^][]*)]`)
 		branch := re_branch.FindString(tree)
-		items = append(items, item{title: branch[1 : len(branch)-1], desc: fmt.Sprintf("%d", i)})
+		branch = branch[1 : len(branch)-1]
+		items = append(items, item{title: branch, desc: get_files_changed(branch)})
 	}
 
 	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
